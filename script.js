cc280cbbc5964a7b3ded7630c9bac4c2f0d1ec3a
B0290077 <vishal.punj@airtel.com>
2024-12-02 21:46:43 +0530

    index html

---
+++ script.js
@@ -0,0 +1,13 @@
+document.getElementById('contactForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('message').value;
+
+    if (name && email && message) {
+        alert(`Thank you, ${name}! Your message has been sent.`);
+        this.reset();
+    } else {
+        alert('Please fill out all fields.');
+    }
+});
